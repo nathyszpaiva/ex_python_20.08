0bf1c9039f32962015b203770423c73c450a52d0
nathyszpaiva <gwenstimez@gmail.com>
2024-08-27 08:43:49 -0300

    Update Exercicio11.py

--- Exercicio11.py
+++ Exercicio11.py
@@ -1,10 +1,12 @@
-quantidade_fotocopias = int(input("Digite a quantidade de fotocópias realizadas: "))
+copias = int(input("Digiteo número de cópias: "))
 
-if quantidade_fotocopias <= 10:
-    custo_total = quantidade_fotocopias * 0.25
-elif quantidade_fotocopias <= 30:
-    custo_total = 10 * 0.25 + (quantidade_fotocopias - 10) * 0.20
-else:
-    custo_total = 10 * 0.25 + 20 * 0.20 + (quantidade_fotocopias - 30) * 0.10
+if copias <= 10:
+    valor = copias * 0.25
+    print(f"Valor: {valor}")
+if(copias > 10 and copias <=30):
+    valor20 = (copias-10) * 0.20 +2.5
+    print(f"Valor:{valor20}")
 
-print(f"O custo total das fotocópias é R$ {custo_total:.2f}.")
+if(copias > 30):
+    valor10 = (copias-30) * 0.10 + 6.5
+    print(f"Valor:{valor10}")
